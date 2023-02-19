1d37d2e9015bcad086b5df1df1e030e5a6df3a0b
Paulsen <acc@paulsen.ooo>
2023-02-19 20:22:49 +0100

    My local changes

--- main.py
+++ main.py
@@ -22,94 +22,38 @@
 # SOFTWARE.
 ####################################################################################
 
-import rp2
-import network
-import ubinascii
-import machine
-import urequests as requests
+# A simple example that:
+# - Connects to a WiFi Network defined by "ssid" and "password"
+# - Performs a GET request (loads a webpage)
+# - Queries the current time from a server
+
+import network   # handles connecting to WiFi
+import urequests # handles making and servicing network requests
 import time
+import machine
 from secrets import secrets
 
-# Set country to avoid possible errors
-rp2.country('DE')
-
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-
-led = machine.Pin('LED', machine.Pin.OUT)
-
-# See the MAC address in the wireless chip OTP
-mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
-print('mac = ' + mac)
 
 # Load login data from different file for safety reasons
 ssid = secrets['ssid']
 pw = secrets['pw']
 botToken = secrets['botToken']
 chatId = secrets['telegramDmUid']
-startupText = 'Pico started!'
-text = 'Door bell activated!'
+startupText = 'Klingelficker aktiviert'
+text = 'Ding Dong!!! Mund auf!!'
 
-# Telegram send message URL
-sendURL = 'https://api.telegram.org/bot' + botToken + '/sendMessage'
-    
-# Send a telegram message to a given user id
-def send_message (chatId, message):
-    response = requests.post(sendURL + "?chat_id=" + str(chatId) + "&text=" + message)
-    # Close to avoid filling up the RAM.
-    response.close()
-
-# Define blinking function for onboard LED to indicate error codes    
-def blink_onboard_led(num_blinks):
-    for i in range(num_blinks):
-        led.on()
-        time.sleep(.2)
-        led.off()
-        time.sleep(.2)
-        
-def is_wifi_connected():
-    wlan_status = wlan.status()
-    if wlan_status != 3:
-        return False
-    else:
-        return True
-
-def connect_wifi():
-    while True:
-        if (is_wifi_connected()):
-            blink_onboard_led(3)
-            led.on()
-            status = wlan.ifconfig()
-            print('ip = ' + status[0])
-            send_message(chatId, startupText)
-            break
-        else:
-            print('WiFi is disconnected. Trying to connect.')
-            led.off()
-            wlan.connect(ssid, pw)
-            time.sleep(3)
-
-# Connect to WiFi
-connect_wifi()
-
-# Setup GPIO pins
-doorBellInput = machine.Pin(18, machine.Pin.IN, machine.Pin.PULL_DOWN)
-loopDelay = 0.25
-buttonDelay = 5
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+led = machine.Pin('LED', machine.Pin.OUT)
+doorBellInput = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_DOWN)
 pressed = 1
 
-while True:
+
+def send_message(message:str):
     try:
-        if (not is_wifi_connected()):
-            connect_wifi()
-        
-        if (doorBellInput.value() == pressed):
-            print('Door bell pressed!')
-            send_message(chatId, text)
-            time.sleep(buttonDelay)
-        
-        time.sleep(loopDelay)
-        
+        if not is_wifi_connected():
+            connect_to_wifi()
+        urequests.get(f"https://api.telegram.org/bot{botToken}/sendMessage?chat_id={chatId}&text="+message)
     except OSError as e:
         print(e)
         led.off()
@@ -118,3 +62,60 @@ while True:
         time.sleep(10)
         led.on()
         pass
+
+def connect_to_wifi():
+    while True:
+        if (is_wifi_connected()):
+            blink_onboard_led(3)
+            led.on()
+            status = wlan.ifconfig()
+            print('ip = ' + status[0])
+            send_message(startupText)
+            break
+        else:
+            print('WiFi is disconnected. Trying to connect.')
+            led.off()
+            wlan.connect(ssid, pw)
+            time.sleep(3)
+
+def is_wifi_connected():
+    wlan_status = wlan.status()
+    if wlan_status != 3:
+        return False
+    else:
+        return True
+
+def blink_onboard_led(num_blinks):
+    for i in range(num_blinks):
+        led.on()
+        time.sleep(.2)
+        led.off()
+        time.sleep(.2)
+
+def is_bell_pressed():
+    if (doorBellInput.value() == pressed):
+        print('Door bell pressed!')
+        return True
+    return False
+
+
+def main_loop():
+    led.on()
+    time.sleep(1)
+    led.off()
+
+    while True:
+        if is_bell_pressed():
+            send_message(text)
+            time.sleep(4)
+        time.sleep(0.1)
+
+
+
+
+
+# Connect to network
+connect_to_wifi()
+
+main_loop()
+
