dca4b494186c86a74099b9465aafa63ceab33d18
Paulsen <acc@paulsen.ooo>
2023-02-21 00:35:49 +0100

    My local changes

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-####################################################################################
-# MIT License
-#
-# Copyright (c) 2022 Maurício C. P. Pessoa
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-####################################################################################
-
 # A simple example that:
 # - Connects to a WiFi Network defined by "ssid" and "password"
 # - Performs a GET request (loads a webpage)
@@ -33,20 +9,29 @@ import time
 import machine
 from secrets import secrets
 
-
-# Load login data from different file for safety reasons
+# secrets
 ssid = secrets['ssid']
 pw = secrets['pw']
 botToken = secrets['botToken']
 chatId = secrets['telegramDmUid']
+# hardware constants
+pin_bell = 0
+pin_led = 'LED'
+time_cooldown_message = 4
+time_cooldown_wifi_retry = 3
+time_loopstep = 0.1 # Update resolution of the pin_bell
+time_cooldown_alive = 30
+# other
 startupText = 'Klingelficker aktiviert'
-text = 'Ding Dong!!! Mund auf!!'
+text = '🔔'
+last_blink = time.time()
 
+
+# other vars
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-led = machine.Pin('LED', machine.Pin.OUT)
-doorBellInput = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_DOWN)
-pressed = 1
+led = machine.Pin(pin_led, machine.Pin.OUT)
+doorBellInput = machine.Pin(pin_bell, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
 
 def send_message(message:str):
@@ -70,13 +55,13 @@ def connect_to_wifi():
             led.on()
             status = wlan.ifconfig()
             print('ip = ' + status[0])
-            send_message(startupText)
+            #send_message(startupText)
             break
         else:
             print('WiFi is disconnected. Trying to connect.')
             led.off()
             wlan.connect(ssid, pw)
-            time.sleep(3)
+            time.sleep(time_cooldown_wifi_retry)
 
 def is_wifi_connected():
     wlan_status = wlan.status()
@@ -93,29 +78,30 @@ def blink_onboard_led(num_blinks):
         time.sleep(.2)
 
 def is_bell_pressed():
-    if (doorBellInput.value() == pressed):
-        print('Door bell pressed!')
+    if (doorBellInput.value() == 1):
+        print(f'{text} [Door bell pressed!]')
         return True
     return False
-
-
-def main_loop():
-    led.on()
-    time.sleep(1)
-    led.off()
-
-    while True:
-        if is_bell_pressed():
-            send_message(text)
-            time.sleep(4)
-        time.sleep(0.1)
-
-
-
 
 
 # Connect to network
 connect_to_wifi()
 
-main_loop()
+led.on()
+time.sleep(1)
+led.off()
 
+while True:
+    if is_bell_pressed():
+        led.on()
+        send_message(text) # send Telegrammessage
+        time.sleep(time_cooldown_message) # cooldown
+        led.off()
+
+    if (time.time() - last_blink > time_cooldown_alive): # show led to signal that it's alive
+        led.on()
+        time.sleep(time_loopstep)
+        led.off()
+        last_blink = time.time()
+    else:
+        time.sleep(time_loopstep)
